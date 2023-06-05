f09a2cd74e6685cd64a18b0d46e39443b53b6de0
leci <leca.deca@mail.ru>
2023-06-05 09:49:36 +0600

    SplashScreen

---
+++ app/src/main/java/com/example/medic/SlpashScreen.java
@@ -0,0 +1,14 @@
+package com.example.medic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SlpashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slpash_screen);
+    }
+}
